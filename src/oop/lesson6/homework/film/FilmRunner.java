673b0d3b61ffcc16a2587114a01483f4c91cc4a9
Xepcoh <al.timchenko@gmail.com>
2016-11-11 15:06:44 +0200

    Change for API Stream

--- src/oop/lesson6/homework/film/FilmRunner.java
+++ src/oop/lesson6/homework/film/FilmRunner.java
@@ -1,10 +1,9 @@
 package oop.lesson6.homework.film;
 
-import oop.lesson6.homework.film.filtering.FilmPredicate;
-import oop.lesson6.homework.film.filtering.Filter;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Xepcoh on 04.11.2016.
@@ -12,12 +11,11 @@ import java.util.List;
 public class FilmRunner {
     public static void main(String[] args) {
         List<Film> films = FilmsFill();
-        List<Genre> genres = new ArrayList<>();
-        genres.add(Genre.HORROR);
-        genres.add(Genre.THRILLER);
 
-        System.out.println(Filter.filter(films, FilmPredicate.conteintsInGenres(new ArrayList<>(genres))));
-        System.out.println(Filter.filter(films, FilmPredicate.withReleaseYearBetween(1990, 2001)));
+        List<Genre> genres = new ArrayList<>(Arrays.asList(Genre.HORROR, Genre.THRILLER));
+
+        System.out.println(filterBetweenYears(films, 1990, 2001));
+        System.out.println(filterInGenres(films, genres));
 
     }
 
@@ -25,23 +23,36 @@ public class FilmRunner {
         Film film1 = new Film("Inferno", 2016);
         film1.addGenre(Genre.ACTION)
                 .addGenre(Genre.THRILLER);
+
         Film film2 = new Film("Trainspotting", 1995);
         film2.addGenre(Genre.DRAMMA);
+
         Film film3 = new Film("Serbuan maut", 2011);
         film3.addGenre(Genre.THRILLER)
                 .addGenre(Genre.ACTION);
+
         Film film4 = new Film("Whiplash", 2013);
         film4.addGenre(Genre.DRAMMA);
+
         Film film5 = new Film("Silent Hill", 2006);
         film5.addGenre(Genre.HORROR);
 
-        List<Film> films = new ArrayList<>();
-        films.add(film1);
-        films.add(film2);
-        films.add(film3);
-        films.add(film4);
-        films.add(film5);
+        List<Film> films = new ArrayList<>(Arrays.asList(film1, film2, film3, film4, film5));
 
         return films;
     }
+
+    private static List<Film> filterInGenres(List<Film> films, List<Genre> genres) {
+        return films.stream()
+                .filter(b -> b.getGenres().stream()
+                        .anyMatch(c -> genres.contains(c)))
+                .collect(Collectors.toList());
+    }
+
+    private static List<Film> filterBetweenYears(List<Film> films, int yearFrom, int yearTo) {
+        return films.stream()
+                .filter(b -> b.getYear() >= yearFrom && b.getYear() <= yearTo)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/oop/lesson6/homework/film/filtering/Predicate.java
+++
@@ -1,8 +0,0 @@
-package oop.lesson6.homework.film.filtering;
-
-/**
- * Created by Xepcoh on 08.11.2016.
- */
-public interface Predicate<T> {
-    public boolean apply(T elem);
-}
